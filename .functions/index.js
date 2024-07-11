95a1e4ef50d971d0fbb5bc409d47be9ff30402e2
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2024-07-10 23:52:15 -0300

    chore: update ssr rewrite

--- .functions/index.js
+++ .functions/index.js
@@ -4,15 +4,15 @@ const { ssr } = require('@ecomplus/storefront-renderer/functions/')
 
 process.env.STOREFRONT_LONG_CACHE = 'true'
 
-exports.rewriteAndSSR = functions.https.onRequest((req, res) => {
-  let path = req.path;
-
-  // Check if the path starts with /festpan-
-  if (path.startsWith('/festpan-')) {
-    // Rewrite /festpan-xxxxxx to /xxxxxx
-    req.url = path.replace('/festpan-', '/');
-  }
-
-  // Forward the request to the SSR function
-  ssr(req, res);
+exports.ssr = functions.https.onRequest((req, res) => {
+    const path = req.path;
+  
+    // Check if the path starts with /festpan-
+    if (path.startsWith('/festpan-')) {
+      // Rewrite /festpan-xxxxxx to /xxxxxx
+      req.url = path.replace('/festpan-', '/');
+    }
+  
+    // Call the SSR function
+    ssr(req, res);
 });
