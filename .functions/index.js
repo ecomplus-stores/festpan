0bd5079e6272d8fae4bb0f40014166f39a538297
Leonardo Matos <leomp120894@gmail.com>
2024-07-12 17:53:17 -0300

    Update index.js

--- .functions/index.js
+++ .functions/index.js
@@ -5,11 +5,8 @@ const { ssr } = require('@ecomplus/storefront-renderer/functions/')
 process.env.STOREFRONT_LONG_CACHE = 'true'
 
 exports.ssr = functions.https.onRequest((req, res) => {
-
-    if (!req.path.startsWith('/fastpan-')) {
-      req.url = req.url.replace(req.path, `/fastpan-${req.path.substring(1)}`)
-    }}
-  
-    // Call the SSR function
-    ssr(req, res);
+  if (!req.path.startsWith('/fastpan-')) {
+    req.url = req.url.replace(req.path, `/fastpan-${req.path.substring(1)}`)
+  }
+  ssr(req, res);
 });
