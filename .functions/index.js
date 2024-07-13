a95d5f297bbd0ab18f38aef55d1feedfeef9b1a5
Leonardo Matos <leomp120894@gmail.com>
2024-07-12 21:17:00 -0300

    Update index.js

--- .functions/index.js
+++ .functions/index.js
@@ -11,6 +11,7 @@ exports.ssr = functions.https.onRequest((req, res) => {
     && !req.path.startsWith('/app/')
     && !req.path.startsWith('/admin/')
     && !req.path.startsWith('/blog')
+    && !req.path.startsWith('/pages')
     && !req.path.startsWith('/404')
   ) {
     const paths = req.path.split('/').slice(1)
