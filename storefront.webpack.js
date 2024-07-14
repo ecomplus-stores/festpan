6620320ff6f2ea0d9017052f4948079640d59fc1
matheusgnreis <matheus@e-com.club>
2024-07-13 22:28:18 -0300

    chore: update component search to filter products

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -7,7 +7,9 @@ module.exports = () => ({
     alias: {
       './methods/set-search-term': path.resolve(dirSearchAlias, 'set-search-term'),
       './html/APrices.html': path.resolve(__dirname, 'template/js/components/APrices.html'),   
-      './js/APrices.js': path.resolve(__dirname, 'template/js/components/APrices.js')
+      './js/APrices.js': path.resolve(__dirname, 'template/js/components/APrices.js'),
+      './js/SearchEngine.js': path.resolve(__dirname, 'template/js/components/SearchEngine.js'),
+      './js/InstantSearch.js': path.resolve(__dirname, 'template/js/components/InstantSearch.js')
     }
   }
 })
