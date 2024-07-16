6e6cde0895b2039583b4ceee5957552412c0b511
Leonardo Matos <leomp120894@gmail.com>
2024-07-16 18:37:34 -0300

    Update index.js

--- .functions/index.js
+++ .functions/index.js
@@ -2,7 +2,7 @@ const functions = require('firebase-functions')
 
 const { ssr } = require('@ecomplus/storefront-renderer/functions/')
 
-process.env.STOREFRONT_LONG_CACHE = 'true'
+process.env.STOREFRONT_LONG_CACHE = 'false'
 
 exports.ssr = functions.https.onRequest((req, res) => {
   const chChar = 'p'
