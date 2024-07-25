3ac560ecdfb0d39ad1ad336c25e049942d205478
Leonardo Matos <leomp120894@gmail.com>
2024-07-25 13:46:09 -0300

    Update storefront.webpack.js

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -6,8 +6,7 @@ module.exports = () => ({
   resolve: {
     alias: {
       './methods/set-search-term': path.resolve(dirSearchAlias, 'set-search-term'),
-      './html/APrices.html': path.resolve(__dirname, 'template/js/components/APrices.html'),   
-      './js/APrices.js': path.resolve(__dirname, 'template/js/components/APrices.js'),
+      './html/APrices.html': path.resolve(__dirname, 'template/js/components/APrices.html'),
       './js/SearchEngine.js': path.resolve(__dirname, 'template/js/components/SearchEngine.js'),
       './js/InstantSearch.js': path.resolve(__dirname, 'template/js/components/InstantSearch.js')
     }
