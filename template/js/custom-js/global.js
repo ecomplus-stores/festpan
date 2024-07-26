03fbcab6b733093382c6e114a8a973cf49676572
Leonardo Matos <leomp120894@gmail.com>
2024-07-26 01:15:10 -0300

    Hard fixing category filter on search always by ID
    Hidding category names and "store" options on search engine filters

---
+++ template/js/custom-js/global.js
@@ -0,0 +1,24 @@
+import * as merge from 'lodash.merge'
+import EcomSearch from '@ecomplus/search-engine'
+
+const fixCategoryIdsFilter = ({ terms }) => {
+  if (
+    terms
+    && terms['categories.name']
+    && /^[0-9a-f]{24}$/.test(terms['categories.name'][0])
+  ) {
+    terms['categories._id'] = terms['categories.name']
+    delete terms['categories.name']
+  }
+}
+
+EcomSearch.dslMiddlewares.push((dsl) => {
+  if (dsl.query && dsl.query.bool) {
+    if (dsl.query.bool.filter) {
+      dsl.query.bool.filter.forEach(fixCategoryIdsFilter)
+    }
+    if (dsl.query.bool.must) {
+      dsl.query.bool.must.forEach(fixCategoryIdsFilter)
+    }
+  }
+})
