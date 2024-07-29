2baefc40e077754c3a5bbb66c9a6c3437cfa5832
Leonardo Matos <leomp120894@gmail.com>
2024-07-28 23:43:31 -0300

    Copying `APrices` and alias for customization

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -3,6 +3,8 @@ const path = require('path')
 module.exports = () => ({
   resolve: {
     alias: {
+      './html/APrices.html': path.resolve(__dirname, 'template/js/components/APrices.html'),
+      './js/APrices.js': path.resolve(__dirname, 'template/js/components/APrices.js')
     }
   }
 })
