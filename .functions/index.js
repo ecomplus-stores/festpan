40394e7558ff6ea53ace18c45ca1e608875a7b7c
Leonardo Matos <leomp120894@gmail.com>
2024-08-10 03:44:56 -0300

    Start fetching domain discount rules at SSR, cache on Firestore

--- .functions/index.js
+++ .functions/index.js
@@ -1,20 +1,55 @@
 const functions = require('firebase-functions')
-
+const { getFirestore } = require('firebase-admin/firestore')
+const axios = require('axios')
 const { ssr } = require('@ecomplus/storefront-renderer/functions/')
 
 process.env.STOREFRONT_LONG_CACHE = 'false'
 
+global.$ssrFetchAndCache = async (
+  url,
+  {
+    maxAge = 300,
+    canUseStale = true,
+    cacheKey,
+    timeout = 4000
+  } = {}
+) => {
+  const key = cacheKey || `${url}`.replace(/\//g, '$').substring(0, 1499)
+  const now = Date.now()
+  const ttlMs = maxAge * 1000
+  const docRef = getFirestore().doc(`ssrFetchCache/${key}`)
+  const docSnap = await docRef.get()
+  const runFetch = async () => {
+    const response = await axios.get(url, { timeout })
+    const { data } = response
+    const cacheVal = { timestamp: now, data }
+    docRef.set(cacheVal)
+    return data
+  }
+  if (docSnap.exists) {
+    const cacheVal = docSnap.data()
+    if (cacheVal.timestamp + ttlMs >= now) {
+      return cacheVal.data
+    }
+    if (canUseStale) {
+      runFetch().catch(console.error)
+      return cacheVal.data
+    }
+  }
+  return runFetch()
+}
+
 exports.ssr = functions.https.onRequest((req, res) => {
   const chChar = 'p'
   if (
-    req.path.length > 1
-    && !req.path.startsWith('/app/')
-    && !req.path.startsWith('/admin/')
-    && !req.path.startsWith('/search')
-    && !req.path.startsWith('/blog')
-    && !req.path.startsWith('/posts/')
-    && !req.path.startsWith('/pages/')
-    && !req.path.startsWith('/404')
+    req.path.length > 1 &&
+    !req.path.startsWith('/app/') &&
+    !req.path.startsWith('/admin/') &&
+    !req.path.startsWith('/search') &&
+    !req.path.startsWith('/blog') &&
+    !req.path.startsWith('/posts/') &&
+    !req.path.startsWith('/pages/') &&
+    !req.path.startsWith('/404')
   ) {
     const paths = req.path.split('/').slice(1)
     if (paths.length < 2) {
@@ -33,5 +68,5 @@ exports.ssr = functions.https.onRequest((req, res) => {
       return
     }
   }
-  ssr(req, res);
-});
+  ssr(req, res)
+})
