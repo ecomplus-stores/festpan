0339b729fc41f9ed0706a704df90ba669d07eb0e
Vitor <vitorrggm@gmail.com>
2025-01-21 09:51:07 -0300

    chore: setting address complement max length to 60 char

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -4,6 +4,7 @@ module.exports = () => ({
   resolve: {
     alias: {
       './html/APrices.html': path.resolve(__dirname, 'template/js/components/APrices.html'),
+      './html/AddressForm.html': path.resolve(__dirname, 'template/js/components/AddressForm.html'),
       './js/APrices.js': path.resolve(__dirname, 'template/js/components/APrices.js'),
       './html/TheProduct.html': path.resolve(__dirname, 'template/js/components/TheProduct.html'),
       './html/ProductGallery.html': path.resolve(__dirname, 'template/js/components/ProductGallery.html'),
