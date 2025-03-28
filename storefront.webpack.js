3696f6b2c1950ccfb8b81d7292bb455b1265d607
Leonardo Matos <leomp120894@gmail.com>
2025-03-28 00:47:37 -0300

    Copying ProductCard to alias

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -7,6 +7,8 @@ module.exports = () => ({
       './html/AddressForm.html': path.resolve(__dirname, 'template/js/components/AddressForm.html'),
       './js/APrices.js': path.resolve(__dirname, 'template/js/components/APrices.js'),
       './html/TheProduct.html': path.resolve(__dirname, 'template/js/components/TheProduct.html'),
+      './html/ProductCard.html': path.resolve(__dirname, 'template/js/components/ProductCard.html'),
+      './html/ProductCard.js': path.resolve(__dirname, 'template/js/components/ProductCard.js'),
       './html/ProductGallery.html': path.resolve(__dirname, 'template/js/components/ProductGallery.html'),
       './js/DiscountApplier.js': path.resolve(__dirname, 'template/js/components/DiscountApplier.js'),
       './../lib/fix-item-final-price': path.resolve(__dirname, 'template/js/custom-js/fix-item-final-price.js')
